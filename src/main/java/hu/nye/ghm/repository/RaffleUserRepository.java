959e45dab3f5498ac55ec6ae5f1c20d67ad1e3c0
Hanna Gudor <hannahgudor@gmail.com>
2025-12-10 19:08:40 +0100

    Bootstrap kinézet és két új html oldal

--- src/main/java/hu/nye/ghm/repository/RaffleUserRepository.java
+++ src/main/java/hu/nye/ghm/repository/RaffleUserRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface RaffleUserRepository extends CrudRepository<RaffleUser, Long> {
     Optional<RaffleUser> findByUserName(String userName);
+    Optional<RaffleUser> findByEmailAddress(String emailAddress);
 }
