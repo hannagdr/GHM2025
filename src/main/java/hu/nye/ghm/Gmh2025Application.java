f29c3e3eaf91b0520c6425696adc905c67ab3da0
hannagdr <hannahgudor@gmail.com>
2025-12-13 13:03:15 +0000

    Raffle információk gombok alap beállítása

--- src/main/java/hu/nye/ghm/Gmh2025Application.java
+++ src/main/java/hu/nye/ghm/Gmh2025Application.java
@@ -2,9 +2,8 @@ package hu.nye.ghm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+//@EnableWebMvc
 @SpringBootApplication
 public class Gmh2025Application {
 
