b310a9e476933cdebea257d460b45222a2c69e45
tamasgergo795 <tamasgergo795@gmail.com>
2025-12-15 15:02:32 +0000

    Felhasználói felület módosítása a Close-hoz

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -14,5 +14,20 @@ document.addEventListener("click", function (e) {
             }
             location.reload();
         });
+    } else if (e.target.classList.contains("close-button")) {
+        const id = e.target.dataset.id;
+        const token = document.querySelector('meta[name="_csrf"]').content;
+        const header = document.querySelector('meta[name="_csrf_header"]').content;
+        fetch(`/raffle/${id}/close`, {
+            method: "POST",
+            headers: {
+                [header]: token
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error("Request failed");
+            }
+            location.reload();
+        });
     }
 });
